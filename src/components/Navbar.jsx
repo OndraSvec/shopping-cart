2667359852a8b785a4df3a68d51dd32ea6232b48
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-08 13:18:57 +0200

    Add navbar elements and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { RiShoppingCartLine } from "react-icons/ri";
+import { RiShoppingCartFill } from "react-icons/ri";
 
 const Navbar = () => (
   <nav className="navBar">
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="products">Products</NavLink>
-    <NavLink to="cart">Cart</NavLink>
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="products">Products</NavLink>
+      </li>
+      <li>
+        <NavLink to="cart">Cart</NavLink>
+      </li>
+    </ul>
+    <div className="logo">
+      <h1>rip current</h1>
+      <p>the surf company</p>
+    </div>
+    <div className="cart">
+      <RiShoppingCartLine />
+    </div>
   </nav>
 );
 
