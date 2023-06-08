f4a760cc689e67c6c96ff807f87a31b59f7d077b
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-08 21:42:56 +0200

    Add placeholder image for HomePage apparel picture

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,22 +1,45 @@
 import { Link } from "react-router-dom";
 import { BsArrowUpRight } from "react-icons/bs";
+import model from "../assets/apparel-model.jpg";
+import modelSmall from "../assets/apparel-model-small.jpg";
+import { useState } from "react";
 
-const HomePage = () => (
-  <div className="homePage">
-    <div className="leftHome">
-      <div className="emptyBox"></div>
-      <div className="emptyBox"></div>
-      <p>Just Dropped</p>
-      <p>summer surf collection</p>
-      <Link to="products">
-        shop now <BsArrowUpRight />
-      </Link>
+const HomePage = () => {
+  const [imgLoaded, setImgLoaded] = useState(false);
+  return (
+    <div className="homePage">
+      <div className="leftHome">
+        <div className="emptyBox"></div>
+        <div className="emptyBox"></div>
+        <p>Just Dropped</p>
+        <p>summer surf collection</p>
+        <Link to="products">
+          shop now <BsArrowUpRight />
+        </Link>
+      </div>
+      <div className="rightHome">
+        <img
+          src={modelSmall}
+          className={`${
+            imgLoaded
+              ? "homePage-model notDisplayed"
+              : "homePage-model displayed"
+          }`}
+        />
+        <img
+          onLoad={() => setImgLoaded(true)}
+          className={`${
+            imgLoaded
+              ? "homePage-model displayed"
+              : "homePage-model notDisplayed"
+          }`}
+          src={model}
+        />
+        <div className="emptyBox"></div>
+        <div className="emptyBox"></div>
+      </div>
     </div>
-    <div className="rightHome">
-      <div className="emptyBox"></div>
-      <div className="emptyBox"></div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default HomePage;
