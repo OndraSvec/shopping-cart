5eea0ed1142c5916d2fdf8275313f1606ba357c2
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-08 16:41:19 +0200

    Add initial HomePage elements and styling

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,3 +1,23 @@
-const HomePage = () => <h2>HomePage</h2>;
+import { Link } from "react-router-dom";
+import { BsArrowUpRight } from "react-icons/bs";
+
+const HomePage = () => (
+  <div className="homePage">
+    <div className="leftHome">
+      <div className="emptyBox"></div>
+      <div className="emptyBox"></div>
+      <p>Just Dropped</p>
+      <p>summer surf collection</p>
+      <Link to="products">
+        shop now <BsArrowUpRight />
+      </Link>
+    </div>
+    <div className="rightHome">
+      <p>placeholder</p>
+      <div className="emptyBox"></div>
+      <div className="emptyBox"></div>
+    </div>
+  </div>
+);
 
 export default HomePage;
