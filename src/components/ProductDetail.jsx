c08d33a03190fe13839168d96c4aa22e15cb034f
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-09 20:54:54 +0200

    Disable eslint rule only-export-components

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import { fetchProduct } from "../utils/api";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export async function loader({ params }) {
   const data = await fetchProduct(params.id);
   return data;
