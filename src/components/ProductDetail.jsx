cc1c152930f056c398a6c09c66037b6f205e1d09
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-10 17:19:23 +0200

    Add initial ProductDetail elements and styling

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,5 +1,8 @@
 import { useLoaderData } from "react-router-dom";
 import { fetchProduct } from "../utils/api";
+import { BsStarFill, BsStarHalf } from "react-icons/bs";
+import LeftPage from "./LeftPage";
+import RightPage from "./RightPage";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export async function loader({ params }) {
@@ -10,6 +13,22 @@ export async function loader({ params }) {
 const ProductDetail = () => {
   const product = useLoaderData();
   console.log(product);
+  return (
+    <div className="productDetail">
+      <LeftPage>
+        <p>{product.brand}</p>
+        <p>${product.price}</p>
+        <p>
+          {product.rating > 4.5 ? <BsStarFill /> : <BsStarHalf />}
+          {product.rating}
+        </p>
+        <p>{product.description}</p>
+      </LeftPage>
+      <RightPage>
+        <img src={product.images[0]} alt={product.title} />
+      </RightPage>
+    </div>
+  );
 };
 
 export default ProductDetail;
